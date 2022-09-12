3aaf584aa5ed9869e555cc58a2f7f9ff500db8c3
Hussain Balhareth <hedejan@yahoo.com>
2022-09-12 06:50:39 +0300

    delete only functions folder

--- functions/kaggle_competition.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import warnings
-import pickle
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.model_selection import train_test_split
-from functions.clean_transform import dump_pickle, load_pickle
